383662ef84f41dde5c1079d1071d247dacd43e73
Michael Siu <michaelsiuhk@gmail.com>
2020-06-27 12:49:30 +0800

    version 2.1

--- lolshop/settings.py
+++ lolshop/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'eyee5s#9c7o)0#x8s)!x&_guz^jmjo
 # DEBUG = True
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = ['mysterious-island-96088.herokuapp.com','127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
